640d00d0d7c4dfc57874a3344e2c44401aca78b3
yeyoung8805 <yyk8805@naver.com>
2022-02-20 18:33:35 +0900

    first commit

---
+++ lib/components/custom_text_form_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class buildSearchAppBar extends StatelessWidget {
+  final String text;
+  const buildSearchAppBar(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Container(
+          child: Column(
+            children: [
+              Text(text),
+              SizedBox(height: 5.0),
+              TextFormField(
+                validator: (value) =>
+                    value!.isEmpty ? "Please enter some text" : null,
+                obscureText: text = "Password" ? true : false,
+                decoration: InputDecoration(
+                    hintText: "Enter $text",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/searchgit_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/components/custom_text_form_field.dart';
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(_buildSearchAppBar()),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildSearchAppBar() {
+    return buildSearchAppBar("Keyword");
+  }
+}
